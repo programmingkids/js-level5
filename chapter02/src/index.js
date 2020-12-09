a408e498efef95b32a0893d0c27cb1d44a3c2d9b
EC2 Default User <hiroshi.829f@gmail.com>
2020-12-09 08:10:46 +0000

    fix chapter02 for master

--- chapter02/src/index.js
+++ chapter02/src/index.js
@@ -1,7 +1,4 @@
-import Phaser from "phaser";
-import Game from './scene/game';
-import LoadScene from './scene/load';
-import MainScene from './scene/main';
+
 
 // 設定情報の連想配列
 const config = {
@@ -16,10 +13,3 @@ const config = {
     },
 };
 
-// Gameオブジェクト作成
-const game = new Game(config);
-// シーンの追加
-game.scene.add("LoadScene",LoadScene);
-game.scene.add("MainScene", MainScene);
-// 最初にLoadSceneを起動
-game.scene.start("LoadScene");
