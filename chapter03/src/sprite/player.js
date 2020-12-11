e0467ea62a7d1fd1862da30aa65f6886948f51f7
EC2 Default User <hiroshi.829f@gmail.com>
2020-12-11 08:30:38 +0000

    fix chapter03 for main

--- chapter03/src/sprite/player.js
+++ chapter03/src/sprite/player.js
@@ -3,38 +3,16 @@ import Phaser from "phaser";
 // 「Phaser.GameObjects.Sprite」を継承した「Player」クラスの作成
 class Player extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, image) {
-        // 親クラスを呼び出す
-        super(scene, x, y, image);
-        // シーンをプロパティとして代入
-        this.scene = scene;
-        // 表示サイズの変更
-        this.setDisplaySize(50, 50);
-        // 最初のフレームを0番にする
-        this.setFrame(0);
-        // メソッドの呼び出し
-        this.createAnimation();
-        // 移動速度の設定
-        this.speed = 5;
+        
+        
+        
     }
     
     update() {
         // カーソルオブジェクトの取得
-        const cursors = this.scene.input.keyboard.createCursorKeys();
-        if(cursors.right.isDown) {
-            this.x += this.speed;
-            this.anims.play('right', true);
-        } else if(cursors.left.isDown) {
-            this.x -= this.speed;
-            this.anims.play('left', true);
-        } else if(cursors.up.isDown) {
-            this.y -= this.speed;
-            this.anims.play('up', true);
-        } else if(cursors.down.isDown) {
-            this.y += this.speed;
-            this.anims.play('down', true);
-        } else {
-            this.anims.stop();
-        }
+        
+        
+        
     }
     
     createAnimation() {
